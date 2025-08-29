c6c2424c8f41838ab361da9618909f5884dcd008
raj07102002 <rajchandrawanshi69@gmail.com>
2025-08-30 05:25:34 +0530

    04

--- 03.py
+++ 03.py
@@ -1,4 +1,13 @@
-def average(a,b,c): 
-    return(a+b+c)/3 
-result=average(10,20,10)
-print("Average of the number=",result)
+# def average(a,b,c): 
+#     return(a+b+c)/3 
+# result=average(10,20,10)
+# print("Average of the number=",result) 
+
+def average (a,b,c):
+    sum= a+b+c 
+    avg= sum/3 
+    # print(avg)
+    return(avg) 
+result=average(1,2,3) 
+
+print("square=",result*2)
---
+++ 04_default_values.py
@@ -0,0 +1,4 @@
+def cal_a(a,b=4): 
+    mul= (a*b)
+    print (mul) 
+cal_a(1)
