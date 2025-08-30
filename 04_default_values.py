634a42083c048146a396db9656d9ae08cf02ef89
raj07102002 <rajchandrawanshi69@gmail.com>
2025-08-30 05:55:29 +0530

    05

--- 04_default_values.py
+++ 04_default_values.py
@@ -1,4 +1,4 @@
-def cal_a(a,b=4): 
+def cal_a(a,b=4): # default values always given later to a as we say in start in last always
     mul= (a*b)
     print (mul) 
 cal_a(1)
---
+++ 05_list.py
@@ -0,0 +1,16 @@
+# nums=['Ram','Balram','sita','Hanuman'] 
+# def call(list): 
+#     print(list) 
+# print(len(nums)) 
+# call()
+# print(len()) 
+
+nums1=['Ram','Balram','sita','Hanuman'] 
+
+def list_sum(nums1): 
+    for item in nums1: 
+        print(item,end=" ")  
+        
+        
+list_sum(nums1) 
+print()
